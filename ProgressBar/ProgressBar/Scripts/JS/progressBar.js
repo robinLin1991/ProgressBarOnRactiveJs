635605499398b59ffc51e64432fb3bea675448bb
robinLin1991 <hongqiang.lin1991@gmail.com>
2016-07-04 00:51:14 +1000

    ProgressBarDemo on RactiveJs
    ProgressBarDemo on RactiveJs

---
+++ ProgressBar/ProgressBar/Scripts/JS/progressBar.js
@@ -0,0 +1,50 @@
+﻿var buttons = [-25, -10, 10, 25];
+var colours = {
+    Green: 'progress-bar-info',
+    Red: 'progress-bar-danger'
+};
+var progressBars = [
+    { id: 1, name: '#progressBar 1', value: 0, colour: colours["Green"] },
+    { id: 2, name: '#progressBar 2', value: 0, colour: colours["Green"] },
+    { id: 3, name: '#progressBar 3', value: 0, colour: colours["Green"] }
+];
+
+
+var ractive = new Ractive({
+    el: '#container',
+    template: '#template',
+    data: {
+        progressBars : progressBars,
+        buttons: buttons,
+        targetBar: progressBars[0]
+    }
+});
+
+ractive.observe('barSelections', function (newValue, oldValue) {
+    if (oldValue !== newValue)
+    {
+        ractive.set('targetBar', progressBars[(newValue - 1)]);
+    }
+});
+
+ractive.on('calculate', function (event, targetBar, value) {
+    if (targetBar)
+    {
+        targetBar.value += value;
+
+        if (targetBar.value > 100)
+        {
+            targetBar.colour = colours["Red"];
+        }
+        else if (targetBar.value < 0)
+        {
+            targetBar.value = 0;
+        }
+        else
+        {
+            targetBar.colour = colours["Green"];
+        }
+
+        ractive.set('progressBars[' + (targetBar.id - 1) + ']', targetBar);
+    }
+});
